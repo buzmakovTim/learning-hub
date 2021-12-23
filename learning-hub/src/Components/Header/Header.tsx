3ca71beaa259c698593b106cb1cb9999e37d3960
buzmakovTim <buzmakov1987@gmail.com>
2021-12-23 13:24:09 -0800

    update

--- learning-hub/src/Components/Header/Header.tsx
+++ learning-hub/src/Components/Header/Header.tsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './Header.module.css'
 import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
 
+    const [location, setLocation] = useState('')
+    
+
     useEffect (() => {
 
             navigator.geolocation.getCurrentPosition(function(position) {
-                console.log("Latitude is :", position.coords.latitude);
-                console.log("Longitude is :", position.coords.longitude);
+                
+                let latitude = position.coords.latitude
+                let longitude = position.coords.longitude
+
+                fetch("https://api.bigdatacloud.net/data/reverse-geocode-client?latitude="+ latitude + "&longitude="+ longitude +"&localityLanguage=en")
+                .then( res => res.json())
+                .then( (data) => {
+                    console.log(data)
+                    
+                    setLocation(data.city + " - " + data.countryCode)
+                })         
+
             });
     });
+
 
         
     return <div className={classes.mainContainer}>
         
-        <p>Header</p>
+        <p>{location}</p>
 
         {/* Navigation */}
         <div className={classes.nav}>
